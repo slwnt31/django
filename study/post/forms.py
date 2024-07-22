e35c8ac16582aa2d8c2b91d56b45a94ed94b4f25
Son <suruna1026@gmail.com>
2024-07-22 18:21:32 +0900

    스타일추가, 작성시간추가

--- study/post/forms.py
+++ study/post/forms.py
@@ -5,3 +5,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model=Post
         fields=['title','content']
+        # widgets = {'title': forms.TextInput(attrs={'class':'form',
+        #                                            'placeholder':'제목을 입력하세요'}),
+        #            'content': forms.Textarea(attrs={'placeholder': '내용을 입력하세요'})
+        #            }
--- study/study/settings.py
+++ study/study/settings.py
@@ -106,17 +106,25 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# settings.py
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
