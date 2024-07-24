01be6b1f09a166e3edaeec8052060d65f043635f
sujin <20241005@sungshin.ac.kr>
2024-07-24 14:35:28 +0900

    second_post login

--- study/users/urls.py
+++ study/users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('signup/', signup, name='signup')
+    path('home/', home, name='home'),
+    path('home/signup/', signup, name='signup'),
+    path('home/login', login, name='login'),
+    path('home/logout', logout, name='logout')
 ]
--- study/users/views.py
+++ study/users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -15,7 +16,7 @@ def signup(request):
             # 새로운 유저 객체를 생성한다.
                 username = request.POST['username'],
                 password = request.POST['password1'],
-                email = request.POST['email'],)
+                email = request.POST['email'])
             # 유저 이름과 비밀번호, 이메일 필드를 입력받은 유저 객체!
             
             auth.login(request, user)
@@ -31,4 +32,39 @@ def signup(request):
 
 # 위 코드는 사용자가 입력한 폼 데이터를 검증받지 않고 DB에 바로 저장하고 있기 때문에
 # 좋은 방법은 아님. 이런 방식은 권장되지 않으므로, 적절한 폼 검증과 사용자 인증 절차
-# 를 추가해서 보안적인 취약성을 방지해야 한다.      
+# 를 추가해서 보안적인 취약성을 방지해야 한다.
+
+
+def login(request):
+    if request.method == 'POST':
+    # POST 요청을 받았을 시 (request.method를 사용해 현재 요청이 POST인지 확인함.)    
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        # request.POST를 사용해 전송된 데이터를 가져온다.
+        
+        user = authenticate(request, usernaer=username, password=password)
+        # authenticate를 사용해 사용자의 정보가 가져온 데이터와 일치하는지 확인한다.
+        
+        if user is not None:
+            auth.login(request, user)
+            # 확인에 성공했다면 auth.login() 함수를 사용해 사용자를 로그인시킨다.
+            
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error':'username or password is incorrect.'})
+            # 확인에 실패했다면 오류 메시지와 함께 로그인 페이지를 다시 렌더링한다.
+    else:
+        return render(request, 'login.html')
+    
+    
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+def home(request):
+    return render(request, 'home.html')
+
+
+# render: 함수나 html을 불러오면서 그 함수나 html에게 데이터 또한 전송할수 있게 함.
+# redirect: 함수나 html을 반환시키는 게 전부임.
